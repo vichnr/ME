e97255d1b334707ff9e4def4aa379155979c03e3
Crislany Victória <crislanyviictoria@gmail.com>
2025-10-08 09:08:07 -0300

    q2

--- q2.py
+++ q2.py
@@ -1,30 +1,34 @@
-def adicionar_gostos(pessoas: list, gostos: list) -> list:
-    gostos_dict = {g['id']: g['gostos'] for g in gostos}
+def adicionar_gostos(pessoas: list, gostos: list):
+    gostos_dict = {gosto['id']: gosto['gostos'] for gosto in gostos}
+    
     resultado = []
     for pessoa in pessoas[:5]:
-        pessoa_copy = pessoa.copy()
-        pessoa_copy['gostos'] = gostos_dict.get(pessoa['id'], [])
-        resultado.append(pessoa_copy)
+        pessoa_com_gostos = pessoa.copy()
+        pessoa_id = pessoa_com_gostos['id']
+        
+        if pessoa_id in gostos_dict:
+            pessoa_com_gostos['gostos'] = gostos_dict[pessoa_id]
+        else:
+            pessoa_com_gostos['gostos'] = []
+        
+        resultado.append(pessoa_com_gostos)
+    
     return resultado
 
 pessoas = [
-    {"id": 1, "nome": "André", "idade": 39},
-    {"id": 2, "nome": "Carlos", "idade": 45},
-    {"id": 3, "nome": "Felipe", "idade": 34},
-    {"id": 4, "nome": "Lucas", "idade": 34},
-    {"id": 5, "nome": "Marcos", "idade": 23},
-    {"id": 6, "nome": "Fernanda", "idade": 31},
-    {"id": 7, "nome": "Julia", "idade": 20}
+    {'id': 1, 'nome': 'André', 'idade': 39},
+    {'id': 2, 'nome': 'Carlos', 'idade': 45},
+    {'id': 3, 'nome': 'Felipe', 'idade': 34},
+    {'id': 4, 'nome': 'Lucas', 'idade': 34},
+    {'id': 5, 'nome': 'Marcos', 'idade': 23}
 ]
 
 gostos = [
-    {"id": 1, "gostos": ["Moda", "Ciência"]},
-    {"id": 2, "gostos": ["História", "Games", "Viagem"]},
-    {"id": 3, "gostos": ["Cinema", "Natureza"]},
-    {"id": 4, "gostos": ["Natureza", "Futebol"]},
-    {"id": 5, "gostos": ["Política", "Dança", "Viagem", "Natureza"]},
-    {"id": 6, "gostos": ["Leitura", "Ciência", "História", "Política", "Natureza"]},
-    {"id": 7, "gostos": ["Carros"]}
+    {"id": 1, "gostos": ['Moda', 'Ciência']},
+    {"id": 2, "gostos": ['História', 'Games', 'Viagem']},
+    {"id": 3, "gostos": ['Cinema', 'Natureza']},
+    {"id": 4, "gostos": ['Natureza', 'Futebol']},
+    {"id": 5, "gostos": ['Política', 'Dança', 'Viagem', 'Natureza']}
 ]
 
 resultado = adicionar_gostos(pessoas, gostos)
